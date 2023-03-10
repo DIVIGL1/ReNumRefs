5d57f9984106ca1aa353c34687aef38e34d3dd1b
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-03-11 00:27:46 +0300

    fixed error

--- mydocfuncs.py
+++ mydocfuncs.py
@@ -28,10 +28,10 @@ def get_all_refs_in_text(doc_object):
     for one_paragraph in doc_object.paragraphs:
         paragraph_text = one_paragraph.text
         start_poz = paragraph_text.find(REFS_START_CHAR_IN_TEXT)
-        end_poz = 0
-        if start_poz > 0:
-            paragraph_text = paragraph_text[(start_poz + 1):]
-            if len(paragraph_text) >= 3:
+        while start_poz != -1:
+            end_poz = 0
+            paragraph_text = paragraph_text[start_poz:]
+            if len(paragraph_text) >= (len(REFS_START_CHAR_IN_TEXT) + 2):
                 for poz, selected_char in enumerate(paragraph_text):
                     if selected_char in REFS_STOP_CHARS:
                         end_poz = poz
@@ -42,6 +42,10 @@ def get_all_refs_in_text(doc_object):
                 if new_mark not in found_refs_in_text:
                     found_refs_in_text.append(new_mark)
 
+            paragraph_text = paragraph_text[(start_poz + 1):]
+
+            start_poz = paragraph_text.find(REFS_START_CHAR_IN_TEXT)
+
     return found_refs_in_text
 
 
@@ -50,7 +54,7 @@ def find_refs_list(doc_object):
     for num_paragraph, one_paragraph in enumerate(doc_object.paragraphs):
         paragraph_text = one_paragraph.text.strip()
         if len(paragraph_text) > 2:
-            if paragraph_text[0] == REFS_START_CHAR and not paragraph_text[1] in REFS_STOP_CHARS:
+            if paragraph_text[0:len(REFS_START_CHAR)] == REFS_START_CHAR and not paragraph_text[len(REFS_START_CHAR)] in REFS_STOP_CHARS:
                 for one_char in REFS_STOP_CHARS:
                     paragraph_text = paragraph_text.split(one_char)[0]
 
@@ -64,8 +68,6 @@ def replace_refs_in_text(doc_object, old_ref, new_ref):
         for one_run in one_paragraph.runs:
             one_run.text = one_run.text.replace(old_ref, new_ref)
 
-    return doc_object
-
 
 def replace_ref_paragraphs(doc_object, ordered_refs, refs_list):
     for num_ref, one_ref in enumerate(ordered_refs):
@@ -75,10 +77,20 @@ def replace_ref_paragraphs(doc_object, ordered_refs, refs_list):
                 new_paragraph_text = one_paragraph[2]
                 new_paragraph_text = f"{num_ref + 1}. {new_paragraph_text[(len(one_ref)):].strip()}"
                 update_paragraph(doc_object.paragraphs[paragraph_poz], new_paragraph_text)
-                doc_object = replace_refs_in_text(doc_object, one_ref, f"{num_ref + 1}")
+                replace_refs_in_text(doc_object, one_ref, f"{num_ref + 1}")
+
                 break
 
-    return doc_object
+
+def get_refs_errors(doc_object, ordered_refs):
+    errors = []
+    for one_ref in ordered_refs:
+        for one_paragraph in doc_object.paragraphs:
+            if one_ref in one_paragraph.text:
+                if one_ref not in errors:
+                    errors.append(one_ref)
+
+    return errors
 
 
 def get_docx_object(file_name):
