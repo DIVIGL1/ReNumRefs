18d953b604cd7dae27124f66287b5ac6f1fa2a7e
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-03-10 23:01:52 +0300

    Added module

--- myconstants.py
+++ myconstants.py
@@ -1,10 +1,11 @@
 REFS_START_CHAR = "#"
 REFS_START_CHAR_IN_TEXT = "[#"
 REFS_STOP_CHARS = ' ,];)'
-
+FILE_EXTENSION = '.docx'
 
 __ALL__ = [
     'REFS_START_CHAR',
     'STOP_CHARS',
     'REFS_START_CHAR_IN_TEXT',
+    'FILE_EXTENSION',
 ]
