275969cf3cb07a4fca3d88380a57c38947b7a3db
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-03-18 16:09:10 +0300

    Added new module.

---
+++ myutils.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+import pickle
+
+
+def save_param(param_name, param_value, filename="saved.pkl"):
+    params_dict = get_all_params(filename="saved.pkl")
+    params_dict[param_name] = param_value
+
+    with open(filename, 'wb') as file_handle:
+        pickle.dump(params_dict, file_handle)
+
+
+def get_all_params(filename="saved.pkl"):
+    if not os.path.isfile(filename):
+        with open(filename, 'wb') as file_handle:
+            pickle.dump({"<ДатаВремя создания>": datetime.datetime.now()}, file_handle)
+
+    with open(filename, "rb") as file_handle:
+        params_dict = pickle.load(file_handle)
+
+    return params_dict
+
+
+def load_param(param_name, default="", filename="saved.pkl"):
+    params_dict = get_all_params(filename=filename)
+
+    return params_dict.get(param_name, default)
